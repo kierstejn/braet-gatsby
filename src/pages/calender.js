9c3e84045ff6ee3ea20faf890e4359e6daf9d033
Sebastian Iversen <kierstejn1994@gmail.com>
2019-10-15 20:48:36 +0200

    connected to CMS

---
+++ src/pages/calender.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from '../components/Layout/Layout';
+
+const calender = ( props ) => (
+    <Layout>
+        Kalender
+    </Layout>
+);
+
+export default calender;
